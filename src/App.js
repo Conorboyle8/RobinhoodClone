d65f2712070a9e6116ce38bcfd4ebad3282678bd
Conorboyle8 <conor_boyle8@yahoo.com>
2024-04-04 13:06:10 -0400

    Table

--- src/App.js
+++ src/App.js
@@ -1,20 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
-//<img src= "robinhoodImage.png" alt="robinhoodImage" width= "50"></img>
+
 function App() {
   return (
-    <div class= "topnav">
-      <a class="active" href="#home"><img src= "robinhoodImage.png" alt="robinhoodImage" width= "50"></img></a>
-      <input type ="text" placeholder = "Search"></input>
-      <a href ="#rewards">Rewards</a>
-      <a href ="#rewards">Investing</a>
-      <a href ="#rewards">Crypto</a>
-      <a href ="#rewards">Spending</a>
-      <a href ="#rewards">Retirement</a>
-      <a href ="#rewards">Notifications</a>
-      <a href ="#rewards">Account</a>
-    </div>
+    <div className = "App">
+      <div class= "topnav">
+        <img src= "robinhoodImage.png" alt="robinhoodImage" width= "50"></img>
+        <a class="active" href="#home"></a>
+        <input type ="text" placeholder = "Search"></input>
+        <a href ="#rewards">Rewards</a>
+        <a href ="#rewards">Investing</a>
+        <a href ="#rewards">Crypto</a>
+        <a href ="#rewards">Spending</a>
+        <a href ="#rewards">Retirement</a>
+        <a href ="#rewards">Notifications</a>
+        <a href ="#rewards">Account</a>
+      </div>
+      <table>
+        <tr>
+          <th>Stocks</th>
+        </tr>
+        <tr>Apple ---- $171.40</tr>
+        <tr>Tesla ---- $173.13</tr>
+        <tr>Nvidia ---- $894.49</tr>
+        <tr>SPY ---- $522.53</tr>
+      </table>
+      </div>
+      
   );
 }
-
 export default App;
